4c280a47e2dae5424aeba54ad91952f6cd731cab
tengfei <tengfei.will@bytedance.com>
2025-07-12 23:19:00 +0800

    chore: 修改子模块设置脚本以使用新的URL格式
    使用 git submodule set-url 命令直接设置子模块URL，替代原有的 sed 替换方式

--- setup-submodule.sh
+++ setup-submodule.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# 替换 .gitmodules 中的占位符为环境变量的值
-sed -i '' "s|SUBMODULE_TOKEN|${SUBMODULE_TOKEN}|g" .gitmodules
+git submodule set-url content "https://${SUBMODULE_TOKEN}@github.com/teefing/obsidian-knowledge.git"
 
 # 初始化并更新 submodule
 git submodule sync --recursive
