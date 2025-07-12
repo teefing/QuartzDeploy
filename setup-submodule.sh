cd1f1010e84b365dd6b3267aed3f814aa00d0594
tengfei <tengfei.will@bytedance.com>
2025-07-12 16:51:05 +0800

    fix: 修正sed命令在macOS上的兼容性问题
    使用空字符串作为备份扩展名参数以兼容macOS的sed命令

--- setup-submodule.sh
+++ setup-submodule.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 替换 .gitmodules 中的占位符为环境变量的值
-sed -i "s|SUBMODULE_TOKEN|${SUBMODULE_TOKEN}|g" .gitmodules
+sed -i '' "s|SUBMODULE_TOKEN|${SUBMODULE_TOKEN}|g" .gitmodules
 
 # 初始化并更新 submodule
 git submodule sync --recursive
